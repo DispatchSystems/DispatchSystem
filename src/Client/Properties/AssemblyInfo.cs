e901e1b27750664f6a943151df3d055fa0ebb2d8
bwookzy <blocckba5her@gmail.com>
2017-12-03 13:29:37 -0800

    Update for v2.1.1

--- src/Client/Properties/AssemblyInfo.cs
+++ src/Client/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Dispatch Terminal")]
+[assembly: AssemblyDescription("The dispatch terminal to pair with the server side version")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Client")]
+[assembly: AssemblyCompany("Dispatch System")]
+[assembly: AssemblyProduct("Dispatch Terminal1")]
 [assembly: AssemblyCopyright("Copyright © BlockBa5her 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
